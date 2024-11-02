51c67a5cb70b3b7a05226c812801185baf4e2521
joaosmoraes <joaoguner01@gmail.com>
2024-11-01 23:00:31 -0300

    .

---
+++ src/Exercicio.kt
@@ -0,0 +1,17 @@
+fun main() {
+    var nome: String = "joaosm"
+    var textSemValor: String
+
+    val calcado: Byte = 40
+
+    val PIB: Long = 186900000000
+    val pop  = 2110000000
+    println(" R$${PIB/pop}")
+    println("R$ ${"%.4f".format(PIB.toDouble() / pop)}") // VALOR COM 4 CASAS DECIMAIS
+
+   //Imprime PIB dividido pela população.
+
+//Esse código define variáveis de tipos diferentes
+// e imprime o resultado da divisão do PIB pela população.
+
+}
---
+++ src/funcoes.kt
@@ -0,0 +1,24 @@
+
+fun converteAnos(anos: Int){
+
+}
+
+
+
+fun main() {
+    // função unit unica linha fazendo multiplicação
+    fun multiplicacao(a: Int, b: Int) = a * b
+    println(multiplicacao(1, 1))
+ val str = "oi"
+    println("tamanho da string é : ${str.length}")
+}
+
+
+
+
+
+
+
+
+
+
