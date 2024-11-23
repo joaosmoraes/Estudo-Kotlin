cb69595e7b5f22a6e05812e2f79de8962221e76d
joaosmoraes <joaoguner01@gmail.com>
2024-11-23 11:25:52 -0300

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	src/funcoes.kt

---
+++ src/ModificadoresVisibilidade.kt
@@ -0,0 +1,4 @@
+/*
+* public: Acesso livre em qualquer parte do código (dentro e fora da classe). É o padrão.
+private: Acesso restrito apenas à própria classe, protegendo dados sensíveis.
+protected: Acesso permitido na classe e em subclasses, mas bloqueado para objetos externos.*/
